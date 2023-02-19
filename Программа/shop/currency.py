39a93d98b87d8d37d951d42437b1730554769db4
Devaree <anya.suvorina@yandex.ru>
2023-02-19 15:17:36 +0300

    Создание модуля игровой валюты

---
+++ Программа/shop/currency.py
@@ -0,0 +1,10 @@
+class Currency():
+    def __init__(self):
+        self.currency = 1000
+    
+    def add(self):
+        self.currency += 1000
+
+    def sub(self):
+        self.currency -= 1000
+        
